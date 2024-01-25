d6bb90d8b9f1118c6777da9be2fbc28675ae477e
Madeleine Carter <cmadeleine02@gmail.com>
2024-01-24 18:58:39 -0600

    first commit

---
+++ ranking_btl.py
@@ -0,0 +1,152 @@
+import random
+import numpy as np
+import gurobipy as gp
+from gurobipy import GRB
+import math
+
+# simulate(n, L, e, gap)
+#
+# INPUT:
+# n = number of elements
+# e = comparison probability
+# L = number of comparisons per pair
+# gap = imposed gap between first element and rest
+#
+# OUTPUT:
+# w_star = vector approximating ideal w
+# l_inf_error = infinity norm error of the approximation vector
+# D_w_error = weighted sum of out of order pairs in the approximation vector
+def simulate(n, L, e, gap):
+    w = make_w(n, gap)
+    # normalize w
+    w_norm = np.asarray(w) / sum(w)
+    P = make_P(n, e, L, w)
+    w_star = lp_algorithm(P, min(w_norm), max(w_norm))
+
+    # l infinity error
+    l_inf_err = max(abs(np.subtract(w_star, w_norm)))/max(w_norm)
+
+    # D_w error
+    D_w_err = 0
+    for i in range(0, n):
+        for j in range(i, n):
+            if ((w_norm[i] - w_norm[j]) * (w_star[i] - w_star[j]) < 0):
+                D_w_err += math.pow((w_norm[i] - w_norm[j]), 2)
+
+    D_w_err = math.sqrt(D_w_err / (2*n*math.pow(np.linalg.norm(w_norm), 2)))
+
+    return w_star, l_inf_err, D_w_err
+
+# make_w(n, delta_k)
+#
+# INPUT:
+# n = number of elements
+# delta_k = imposed gap between first element and rest
+#
+# OUTPUT:
+# w = ground truth score vector
+def make_w(n, delta_k):
+    w = [0] * n
+    for i in range(0, n):
+        w[i] = random.random() * 0.5 + 0.5
+
+    w_min = min(w)
+
+    for i in range(0, n):
+        if (w[i] > w_min):
+            w[i] += delta_k
+
+    return w
+
+# make_P(n, e, L, w)
+#
+# INPUT:
+# n = number of elements
+# e = comparison probability
+# L = number of comparisons per pair
+# w = ground truth score vector
+#
+# OUTPUT:
+# P_btl: synthetic data matrix of size nxn
+def make_P(n, e, L, w):
+
+    P_btl = np.zeros((n, n))
+
+    # fill in probability matrix
+    for i in range(0, n):
+        for j in range(i, n):
+            # diagonals (i beats i) should have probability of 1/2
+            if (i==j):
+                P_btl[i][j] = 1/2
+            # if i and j get compared:
+            elif (random.random() < e):
+                # generate a number of wins according to each model's respective probability function
+                wins = np.random.binomial(L, (w[i] / (w[i] + w[j])))
+                # record the probability of wins and the inverse in each matrix ([i][j] and [j][i])
+                P_btl[i][j] = wins / L
+                P_btl[j][i] = 1 - P_btl[i][j]
+            # else: P_ij = 0
+            else:
+                P_btl[i][j] = 0
+
+    return P_btl         
+
+# lp_algorithm(P, w_min, w_max)
+#
+# INPUT:
+# P = synthetic data matrix
+# w_min = minimum score in w vector
+# w_max = maximum score in w vector
+#
+# OUTPUT:
+# w_star = vector approximating ideal w
+def lp_algorithm(P, w_min, w_max):
+
+    n = P.shape[0]
+
+    m_btl = gp.Model("BTL")
+    # x: 1D vector (used to approximate s)
+    x_btl = m_btl.addMVar((n, 1), lb=math.log(w_min), ub=math.log(w_max), vtype=GRB.CONTINUOUS, name="x")
+    # z: nxn matrix, z = Y - P
+    z_btl = m_btl.addMVar((n, n), lb=0.0, vtype=GRB.CONTINUOUS, name="z")
+
+    zeroMat = np.zeros((n, n))
+    eVec = np.full((n, 1), 1)
+    # keep a map of nonzero entries; useful for speeding up algorithm
+    mask = np.zeros((n, n))
+
+    # apply link function to P
+    P_link = np.zeros((n, n))
+    for i in range(0, n):
+        for j in range(0, n):
+            # we assume no probabilities are zero unless they were not compared
+            if (P[i][j] == 0):
+                P_link[i][j] = 0
+            else:
+                # 1 / (1 - x) undefined
+                if (P[i][j] == 1):
+                    P_link[i][j] = 0
+                    continue
+                P_link[i][j] = math.log(P[i][j] / (1 - P[i][j]))
+                if (i != j):
+                    mask[i][j] = 1
+
+    # constraints
+    m_btl.addConstr((x_btl @ eVec.T) - (eVec @ x_btl.T) + z_btl - P_link >= zeroMat)
+    m_btl.addConstr((x_btl @ eVec.T) - (eVec @ x_btl.T) - z_btl - P_link <= zeroMat)
+
+    # constraints include entire matrix, but we only care when link[i][j] != 0 and i != j; so apply a mask: np.multiply(z, mask)
+    # this way, the optimization problem can ignore zero entries
+    m_btl.setObjective((z_btl * mask).sum(), GRB.MINIMIZE)
+
+    m_btl.optimize()
+
+    # initialize approximation vector of ideal w
+    w_star = []
+
+    #undo log ->   x = log(w) => w = 2^x
+    for i in range(0, n):
+        w_star.append(pow(math.e, x_btl.X[i])[0])
+
+    w_star = w_star / sum(w_star)
+    return w_star
---
+++ ranking_thu.py
@@ -0,0 +1,153 @@
+import random
+import numpy as np
+import gurobipy as gp
+from gurobipy import GRB
+import math
+from scipy.stats import norm
+
+# simulate(n, L, e, gap)
+#
+# INPUT:
+# n = number of elements
+# e = comparison probability
+# L = number of comparisons per pair
+# gap = imposed gap between first element and rest
+#
+# OUTPUT:
+# s_star = vector approximating ideal s
+# l_inf_error = infinity norm error of the approximation vector
+# D_w_error = weighted sum of out of order pairs in the approximation vector
+def simulate(n, L, e, gap):
+    s = make_s(n, gap)
+    # normalize s
+    s_norm = np.asarray(s) / sum(s)
+    P = make_P(n, e, L, w)
+    s_star = lp_algorithm(P, min(s_norm), max(s_norm))
+
+    # l infinity error
+    l_inf_err = max(abs(np.subtract(s_star, s_norm)))/max(s_norm)
+
+    # D_w error
+    D_w_err = 0
+    for i in range(0, n):
+        for j in range(i, n):
+            if ((s_norm[i] - s_norm[j]) * (s_star[i] - s_star[j]) < 0):
+                D_w_err += math.pow((s_norm[i] - s_norm[j]), 2)
+
+    D_w_err = math.sqrt(D_w_err / (2*n*math.pow(np.linalg.norm(s_norm), 2)))
+
+    return s_star, l_inf_err, D_w_err
+
+# make_s(n, delta_k)
+#
+# INPUT:
+# n = number of elements
+# delta_k = imposed gap between first element and rest
+#
+# OUTPUT:
+# s = ground truth score vector
+def make_s(n, delta_k):
+    s = [0] * n
+    for i in range(0, n):
+        s[i] = random.random() * 0.5 + 0.5
+
+    s_min = min(s)
+
+    for i in range(0, n):
+        if (s[i] > s_min):
+            s[i] += delta_k
+
+    return s
+
+# make_P(n, e, L, s)
+#
+# INPUT:
+# n = number of elements
+# e = comparison probability
+# L = number of comparisons per pair
+# s = ground truth score vector
+#
+# OUTPUT:
+# P_btl: synthetic data matrix of size nxn
+def make_P(n, e, L, s):
+
+    P_thu = np.zeros((n, n))
+
+    # fill in probability matrix
+    for i in range(0, n):
+        for j in range(i, n):
+            # diagonals (i beats i) should have probability of 1/2
+            if (i==j):
+                P_thu[i][j] = 1/2
+            # if i and j get compared:
+            elif (random.random() < e):
+                # generate a number of wins according to each model's respective probability function
+                wins = np.random.binomial(L,  norm.cdf(s[i] - s[j]))
+                # record the probability of wins and the inverse in each matrix ([i][j] and [j][i])
+                P_thu[i][j] = wins / L
+                P_thu[j][i] = 1 - P_thu[i][j]
+            # else: P_ij = 0
+            else:
+                P_thu[i][j] = 0
+
+    return P_thu
+
+# lp_algorithm(P, s_min, s_max)
+#
+# INPUT:
+# P = synthetic data matrix
+# s_min = minimum score in s vector
+# s_max = maximum score in s vector
+#
+# OUTPUT:
+# s_star = vector approximating ideal s
+def lp_algorithm(P, s_min, s_max):
+
+    n = P.shape[0]
+
+    m_thu = gp.Model("Thurstone")
+    # x: 1D vector (used to approximate s)
+    x_thu = m_thu.addMVar((n, 1), lb=math.log(s_min), ub=math.log(s_max), vtype=GRB.CONTINUOUS, name="x")
+    # z: nxn matrix, z = Y - P
+    z_thu = m_thu.addMVar((n, n), lb=0.0, vtype=GRB.CONTINUOUS, name="z")
+
+    zeroMat = np.zeros((n, n))
+    eVec = np.full((n, 1), 1)
+    # keep a map of nonzero entries; useful for speeding up algorithm
+    mask = np.zeros((n, n))
+
+    # apply link function to P
+    P_link = np.zeros((n, n))
+    for i in range(0, n):
+        for j in range(0, n):
+            # we assume no probabilities are zero unless they were not compared
+            if (P[i][j] == 0):
+                P_link[i][j] = 0
+            else:
+                # 1 / (1 - x) undefined
+                if (P[i][j] == 1):
+                    P_link[i][j] = 0
+                    continue
+                P_link[i][j] = norm.ppf(P[i][j])
+                if (i != j):
+                    mask[i][j] = 1
+
+    # constraints
+    m_thu.addConstr((x_thu @ eVec.T) - (eVec @ x_thu.T) + z_thu - P_link >= zeroMat)
+    m_thu.addConstr((x_thu @ eVec.T) - (eVec @ x_thu.T) - z_thu - P_link <= zeroMat)
+
+    # constraints include entire matrix, but we only care when link[i][j] != 0 and i != j; so apply a mask: np.multiply(z, mask)
+    # this way, the optimization problem can ignore zero entries
+    m_thu.setObjective((z_thu * mask).sum(), GRB.MINIMIZE)
+
+    m_thu.optimize()
+
+    # initialize approximation vector of ideal s
+    s_star = []
+
+    # read in values directly
+    for i in range(0, n):
+        s_star.append(x_thu.X[i][0])
+
+    s_star = s_star / sum(s_star)
+    return s_star
